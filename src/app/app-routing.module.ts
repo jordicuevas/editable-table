6a1e97c545f12f9c56ec4779bd3b882190085920
Jordi Cuevas <jordicuevas@gmail.com>
2019-11-29 14:42:18 -0400

    editable table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'table',
+    path: '',
     loadChildren: './tabla-editable/tabla-editable.module#TablaEditableModule'
   }
 ];
